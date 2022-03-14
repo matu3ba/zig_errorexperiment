93dce5dfecd13fa9bdf272585e84770ecef554df
Jan Philipp Hafer <jan.hafer@rwth-aachen.de>
2022-03-14 01:14:42 +0100

    comptime gen of error table looks like it is not possible

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,39 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+fn printErrorSet(comptime fun: anytype) void {
+    const info = @typeInfo(@TypeOf(fun));
+    const ret_type = info.Fn.return_type.?;
+
+    // looks like there is currently no way to comptime create error set tables with integers
+    inline for (@typeInfo(@typeInfo(ret_type).ErrorUnion.error_set).ErrorSet.?) |reterror| {
+        std.debug.print("{s}\n", .{reterror.name});
+        std.debug.print("{s}\n", .{@errorToInt(reterror)});
+        //std.debug.print("{s}\n", .{@errorToInt(reterror)});
+        //std.debug.print("{d}\n", .{@errorToInt(error.test123)});
+        //std.debug.print("{s}\n", .{@typeInfo(error.test123)});
+    }
+    //std.debug.print("{s}\n", .{info});
+    //std.debug.print("{s}\n", .{ret_type});
+}
+//const all_errs =
+//DiskQuota
+//FileTooBig
+//InputOutput
+//NoSpaceLeft
+//AccessDenied
+//BrokenPipe
+//SystemResources
+//OperationAborted
+//NotOpenForWriting
+//WouldBlock
+//ConnectionResetByPeer
+//Unexpected
+
+test "printErrors" {
+    printErrorSet(main);
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+pub fn main() !void {
+    const stdout = std.io.getStdOut();
+    try stdout.writeAll("All your codebase are belong to us.\n");
 }
